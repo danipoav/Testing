d0c018465cf674409857a923d647671cb25aaf2b
danipoav <dapoav2002@gmail.com>
2024-12-25 13:42:25 +0100

    Changing to TypeScript

--- test/index.ts
+++ test/index.ts
@@ -1,52 +1,60 @@
 class Room {
-  constructor(name, rate, discount = 0) {
+  name: string;
+  bookings: Booking[];
+  rate: number;
+  discount: number;
+
+  constructor(name: string, rate: number, discount: number = 0) {
     this.name = name;
     this.bookings = [];
     this.rate = rate;
     this.discount = discount;
   }
 
-  isOccupied(date) {
+  isOccupied(date: Date): boolean {
     return this.bookings.some(
       (booking) =>
         date >= booking.checkIn && date <= booking.checkOut
-    )
-
+    );
   }
 
-  occupancyPercentage(startDate, endDate) {
-    const start = new Date(startDate)
-    const end = new Date(endDate)
+  occupancyPercentage(startDate: Date, endDate: Date): string {
+    const start = new Date(startDate);
+    const end = new Date(endDate);
     let totalDays = 0;
     let occupancyDays = 0;
 
-    let currentDate = new Date(start)
+    let currentDate = new Date(start);
     while (currentDate <= end) {
       totalDays++;
-      currentDate.setDate(currentDate.getDate() + 1)
+      currentDate.setDate(currentDate.getDate() + 1);
     }
 
-    this.bookings.forEach(booking => {
+    this.bookings.forEach((booking) => {
       const bookingStart = new Date(booking.checkIn);
       const bookingEnd = new Date(booking.checkOut);
 
-      const overStart = bookingStart > start ? bookingStart : start
-      const overEnd = bookingEnd < end ? bookingEnd : end
+      const overStart = bookingStart > start ? bookingStart : start;
+      const overEnd = bookingEnd < end ? bookingEnd : end;
 
       if (overStart <= overEnd) {
-        let date = new Date(overStart)
+        let date = new Date(overStart);
         while (date <= overEnd) {
           occupancyDays++;
-          date.setDate(date.getDate() + 1)
+          date.setDate(date.getDate() + 1);
         }
       }
-    })
-    const occupancyPercentage = ((occupancyDays / totalDays) * 100).toFixed(2)
+    });
 
+    const occupancyPercentage = ((occupancyDays / totalDays) * 100).toFixed(2);
     return occupancyPercentage;
   }
 
-  static totalOccupancyPercentage(rooms, startDate, endDate) {
+  static totalOccupancyPercentage(
+    rooms: Room[],
+    startDate: Date,
+    endDate: Date
+  ): string {
     const start = new Date(startDate);
     const end = new Date(endDate);
     let totalDays = 0;
@@ -59,33 +67,48 @@ class Room {
 
     let totalOccupiedDays = 0;
     rooms.forEach((room) => {
-      const occupancyPercentage = room.occupancyPercentage(startDate, endDate);
+      const occupancyPercentage = parseFloat(
+        room.occupancyPercentage(startDate, endDate)
+      );
       totalOccupiedDays += (occupancyPercentage / 100) * totalDays;
     });
 
     const totalDaysRooms = rooms.length * totalDays;
-
-    return (totalOccupiedDays / totalDaysRooms * 100).toFixed(2);
+    return ((totalOccupiedDays / totalDaysRooms) * 100).toFixed(2);
   }
 
-
-  static availableRooms(rooms, startDate, endDate) {
-    const start = startDate;
-    const end = endDate;
-
-    return rooms.filter(room => {
-      return !room.bookings.some(booking => {
+  static availableRooms(
+    rooms: Room[],
+    startDate: Date,
+    endDate: Date
+  ): Room[] {
+    return rooms.filter((room) => {
+      return !room.bookings.some((booking) => {
         const bookingStart = booking.checkIn;
         const bookingEnd = booking.checkOut;
 
-        return start <= bookingStart && end >= bookingEnd
+        return startDate <= bookingStart && endDate >= bookingEnd;
       });
     });
   }
 }
 
 class Booking {
-  constructor(name, email, checkIn, checkOut, discount, room) {
+  name: string;
+  email: string;
+  checkIn: Date;
+  checkOut: Date;
+  discount: number;
+  room: Room;
+
+  constructor(
+    name: string,
+    email: string,
+    checkIn: Date,
+    checkOut: Date,
+    discount: number,
+    room: Room
+  ) {
     this.name = name;
     this.email = email;
     this.checkIn = checkIn;
@@ -94,7 +117,7 @@ class Booking {
     this.room = room;
   }
 
-  get fee() {
+  get fee(): number {
     const roomRate = this.room.rate;
     const roomDiscount = this.room.discount;
     const bookingDiscount = this.discount;
@@ -105,17 +128,15 @@ class Booking {
     const bookingRateDiscount = (discountedRate * bookingDiscount) / 100;
     const finalRate = discountedRate - bookingRateDiscount;
 
-
     let totalDays = 0;
-
     let startDate = new Date(this.checkIn);
     while (startDate <= this.checkOut) {
       totalDays++;
       startDate.setDate(startDate.getDate() + 1);
     }
 
-    return finalRate * totalDays
+    return finalRate * totalDays;
   }
 }
 
-module.exports = { Room, Booking };
+export { Room, Booking };
